70ce339430f94b09221fd512ad31d4560b00a201
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-05-08 14:04:26 -0700

    Added horizontal input to player movement.

--- WashAway/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ WashAway/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -6,9 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     protected const float ACCELERATION_DUE_TO_GRAVITY = 9.8f;
 
-    [SerializeField] protected float maxHorizontalSpeed;
-    [SerializeField] protected float accelerationScale;
-    [SerializeField] protected float gravityScale;
+    [SerializeField] protected float maxHorizontalSpeed = 10;
+    [SerializeField] protected float accelerationScale = 10;
+    [SerializeField] protected float gravityScale = 1;
 
     protected Vector3 _velocity;
 
@@ -20,6 +20,23 @@ public class PlayerMovement : MonoBehaviour
     protected void Move(float deltaTime)
     {
         _velocity += ACCELERATION_DUE_TO_GRAVITY * Vector3.down * gravityScale * deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _velocity += accelerationScale * Vector3.right * deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _velocity += accelerationScale * Vector3.left * deltaTime;
+        }
+
+        Vector2 horizontalVelocity = _velocity;
+        if (horizontalVelocity.magnitude > maxHorizontalSpeed)
+        {
+            horizontalVelocity = horizontalVelocity.normalized * maxHorizontalSpeed;
+            _velocity = new Vector3(horizontalVelocity.x, horizontalVelocity.y, _velocity.z);
+        }
 
         transform.position += _velocity * deltaTime;
     }
