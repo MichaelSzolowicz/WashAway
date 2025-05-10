959d0e531febfb3008efa5f5db092de0f1212f1d
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-05-10 16:06:35 -0700

    Created line collision scripts.

---
+++ WashAway/Assets/Scripts/LineCollision/LineCollider.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCollider : MonoBehaviour
+{
+    protected List<Line> _lines = new List<Line>();
+
+
+}
