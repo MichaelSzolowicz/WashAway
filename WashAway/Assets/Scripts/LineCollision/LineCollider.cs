f5a5d6297feb17577ec0664da4a2915eaf4993f2
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-05-15 17:22:13 -0700

    Added colors for when line colliders are selected and deselected.

--- WashAway/Assets/Scripts/LineCollision/LineCollider.cs
+++ WashAway/Assets/Scripts/LineCollision/LineCollider.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [ExecuteInEditMode]
 public class LineCollider : MonoBehaviour
@@ -14,6 +15,13 @@ public class LineCollider : MonoBehaviour
 
     [SerializeField] protected bool visibleInGame = true;
     [SerializeField] protected bool visibleInEditor = true;
+
+    [SerializeField] protected Color defaultColor = Color.white;
+    [SerializeField] protected Color selectedColor = Color.yellow;
+    [SerializeField] protected float pointSize = .01f;
+    [SerializeField] protected float lineWidth = 1;
+
+    protected bool isSelected = false;
 
     public int Length
     {
@@ -36,6 +44,18 @@ public class LineCollider : MonoBehaviour
         get
         {
             return width;
+        }
+    }
+
+    public bool IsSelected
+    {
+        get
+        {
+            return isSelected;
+        }
+        set
+        {
+            isSelected = value;
         }
     }
 
@@ -78,24 +98,29 @@ public class LineCollider : MonoBehaviour
         _points[index] = rotatedPoint;
     }
 
+    protected void OnDestroy()
+    {
+        
+    }
+
     protected void OnDrawGizmos()
     {
         if (!visibleInEditor && !visibleInGame) return;
         else if (SceneView.currentDrawingSceneView == null && !visibleInGame) return;
         else if(SceneView.currentDrawingSceneView != null && !visibleInEditor) return;
 
-        Handles.color = Color.blue;
+        Color useColor = isSelected ? selectedColor : defaultColor;
+        Handles.color = useColor;
         for (int i = 0; i < Length; i++)
         {
             Vector3 position = GetPointWorldSpace(i);
 
-            Handles.DrawSolidDisc(position, -Vector3.forward, HandleUtility.GetHandleSize(position) * .1f);
+            Gizmos.DrawIcon(position, "point", false, useColor);
         }
 
         for (int p1 = 0, p2 = 1; p2 < Length; p1++, p2++)
         {
-            Handles.DrawLine(GetPointWorldSpace(p1), GetPointWorldSpace(p2), width);
+            Handles.DrawLine(GetPointWorldSpace(p1), GetPointWorldSpace(p2), lineWidth);
         }
     }
 }
-
