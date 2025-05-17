f9c54551a828c46b9d8eb77bcefdffc43f5f2724
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-05-17 12:30:16 -0700

    Updated line collider to use new line point class.

--- WashAway/Assets/Scripts/LineCollision/LinePoint.cs
+++ WashAway/Assets/Scripts/LineCollision/LinePoint.cs
@@ -21,4 +21,22 @@ public class LinePoint
             _position = value;
         }
     }
+
+    public float x
+    {
+        get { return _position.x; }
+        set
+        {
+            _position.x = value;
+        }
+    }
+
+    public float y
+    {
+        get { return _position.y; }
+        set
+        {
+            _position.y = value;
+        }
+    }
 }
