40529395890a997c0a9b700f53c520d535a377c2
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-07-19 17:15:34 -0700

    Setup game to pause when finish line is reached.

---
+++ WashAway/Assets/Game/Scripts/GameState/GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameState
+{
+    private static bool paused = false;
+
+    public static bool  Paused { get { return paused; } set { paused = value; } }
+}
