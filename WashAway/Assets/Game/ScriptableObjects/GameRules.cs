4685d9ff0b6e09b9b3a2f30f0339b1486f239414
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-07-20 18:38:11 -0700

    Working on tools to automatically update victory screen in scene based on scriptable object value.

--- WashAway/Assets/Game/ScriptableObjects/GameRules.cs
+++ WashAway/Assets/Game/ScriptableObjects/GameRules.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewGameRules", menuName = "ScriptableObjects/GameRules")]
@@ -8,4 +9,14 @@ public class GameRules : ScriptableObject
     [SerializeField] private GameObject pauseScreen;
 
     public GameObject PauseScreen { get { return pauseScreen; } }
+
+    public delegate void OnValidateGameRules();
+    public event OnValidateGameRules onValidateGameRules;
+
+    private void OnValidate()
+    {
+        Debug.Log("Validate " + name);
+
+        onValidateGameRules?.Invoke();
+    }
 }
