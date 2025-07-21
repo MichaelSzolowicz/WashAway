174b4258b826be0207ee03b616a89caefada2686
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-07-21 00:54:47 -0700

    Instantiate UI at runtime. Rolling back editor tools.

--- WashAway/Assets/Game/ScriptableObjects/GameRules.cs
+++ WashAway/Assets/Game/ScriptableObjects/GameRules.cs
@@ -6,17 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewGameRules", menuName = "ScriptableObjects/GameRules")]
 public class GameRules : ScriptableObject
 {
+    [SerializeField] private GameObject canvas;
     [SerializeField] private GameObject pauseScreen;
 
     public GameObject PauseScreen { get { return pauseScreen; } }
-
-    public delegate void OnValidateGameRules();
-    public event OnValidateGameRules onValidateGameRules;
-
-    private void OnValidate()
-    {
-        Debug.Log("Validate " + name);
-
-        onValidateGameRules?.Invoke();
-    }
+    public GameObject Canvas { get { return canvas; } }
 }
