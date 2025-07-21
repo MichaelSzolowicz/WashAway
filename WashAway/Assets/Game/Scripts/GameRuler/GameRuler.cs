9b847ce3fdd0ae1c696961234c498122d87aa7d9
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-07-21 00:54:47 -0700

    Instantiate UI at runtime. Rolling back editor tools.

--- WashAway/Assets/Game/Scripts/GameRuler/GameRuler.cs
+++ WashAway/Assets/Game/Scripts/GameRuler/GameRuler.cs
@@ -1,116 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-#if UNITY_EDITOR
-using UnityEditor.SceneManagement;
-#endif
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class GameRuler : MonoBehaviour
 {
     [SerializeField] private GameRules gameRules;
 
-    [SerializeField] private bool forceValidateTest;
-
     [SerializeField] [HideInInspector] private GameObject canvas;
     [SerializeField] [HideInInspector] private GameObject pauseScreen;
-    [SerializeField][HideInInspector] private GameObject pauseScreenTemplate;
 
-    private bool spawnCanvas = false;
-    private bool spawnPauseScreen = false;
 
-    private void Update()
+    private void Start()
     {
-        if(Application.isPlaying)
+        if(gameRules != null)
         {
-            if (GameState.Paused)
-            {
-                pauseScreen.SetActive(true);
-            }
-            else
-            {
-                pauseScreen.SetActive(false);
-            }
-        }
-        else
-        {
-            if(spawnCanvas)
+            canvas = Instantiate(gameRules.Canvas);
+            if(canvas == null)
             {
                 canvas = new GameObject();
                 canvas.name = "Canvas";
-
-                Canvas canvasComponent = canvas.AddComponent<Canvas>();
-                canvasComponent.renderMode = RenderMode.ScreenSpaceOverlay;
-
-                spawnCanvas = false;
-
-                print("Spawn Canvas");
             }
 
-            if(spawnPauseScreen)
+            pauseScreen = Instantiate(gameRules.PauseScreen, canvas.transform, false);
+            if(pauseScreen == null)
             {
-                SpawnPauseScreen();
+                pauseScreen = new GameObject();
+                pauseScreen.name = "PauseScreen";
             }
-
-#if UNITY_EDITOR
-            EditorSceneManager.SaveScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
-#endif
         }
     }
 
-    private void SpawnPauseScreen()
+    private void Update()
     {
-        if (pauseScreen != null)
+        if (GameState.Paused)
         {
-            DestroyImmediate(pauseScreen);
-        }
-
-        if (gameRules != null 
-            && gameRules.PauseScreen != null)
-        {
-            pauseScreenTemplate = gameRules.PauseScreen;
-            pauseScreen = Instantiate(pauseScreenTemplate);
+            pauseScreen.SetActive(true);
         }
         else
         {
-            pauseScreen = pauseScreenTemplate = new GameObject();
-            pauseScreen.name = "PauseScreen";
-        }
-
-        pauseScreen.transform.SetParent(canvas.transform, false);
-        pauseScreen.SetActive(false);
-
-        spawnPauseScreen = false;
-
-        print("Spawn Pause Screen");
-    }
-
-    private void ValidateSubobjects()
-    {
-        spawnCanvas = canvas == null;
-
-        if(gameRules != null)
-        {
-            spawnPauseScreen = gameRules.PauseScreen != pauseScreenTemplate || pauseScreen == null;
-        }
-    }
-
-    private void OnValidate()
-    {
-        if(gameRules != null)
-        {
-            gameRules.onValidateGameRules += ValidateSubobjects;
-        }
-
-        ValidateSubobjects();
-    }
-
-    private void OnDestroy()
-    {
-        if(gameRules != null)
-        {
-            gameRules.onValidateGameRules -= ValidateSubobjects;
+            pauseScreen.SetActive(false);
         }
     }
 }
