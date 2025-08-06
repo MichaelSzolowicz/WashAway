e50a9b7352eb68cd2ca4338364e9423f20d7fcdc
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-08-05 18:32:49 -0700

    Added button functionality to pause and victory screens.

---
+++ WashAway/Assets/Game/Scripts/UI/ButtonUtilities.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUtilities : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        StartCoroutine(ReloadSceneAsync());
+    }
+
+    private IEnumerator ReloadSceneAsync()
+    {
+        AsyncOperation asyncReload = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        while(!asyncReload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void Feedback()
+    {
+        Application.OpenURL("www.michaelszolowicz.com");
+    }
+}
