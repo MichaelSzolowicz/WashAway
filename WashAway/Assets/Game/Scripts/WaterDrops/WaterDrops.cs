632a95cfa7b630585f463f2035e6591090b6eafd
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-08-16 17:05:08 -0700

    Water drop spawn delay.

--- WashAway/Assets/Game/Scripts/WaterDrops/WaterDrops.cs
+++ WashAway/Assets/Game/Scripts/WaterDrops/WaterDrops.cs
@@ -6,10 +6,15 @@ public class WaterDrops : MonoBehaviour
 {
     [SerializeField] private WAArtist artist;
     [SerializeField] private Texture texture;
-    //private List<PaintedSprite> paintedSprites;
-    [SerializeField] private float numDrops;
-    private List<Transform> drops = new List<Transform>();
+    [SerializeField] private int numDrops;
+    [SerializeField] private float dropLifetime;
+    private List<WaterDrop> drops = new List<WaterDrop>();
+    private List<PaintedSprite> paintedSprites = new List<PaintedSprite>(); 
     [SerializeField] private float pixelsPerMeter;
+    private int numEnabledDrops = 0;
+    [SerializeField] private float spawnDelay = .5f;
+
+    private float elapsedTime = 0;  
 
     private void Start()
     {
@@ -19,20 +24,51 @@ public class WaterDrops : MonoBehaviour
 
             GameObject newDrop = new GameObject();
             newDrop.name = spriteName;
-            newDrop.transform.localScale = transform.localScale;
+            newDrop.transform.localScale = transform.lossyScale;
+            newDrop.transform.position = transform.position;
+            WaterDrop newDropComponent = newDrop.AddComponent<WaterDrop>();
+            drops.Add(newDropComponent);
 
             PaintedSprite newPaintedSprite = new PaintedSprite();
             newPaintedSprite.tag = spriteName;
             newPaintedSprite.sourceTexture = texture;
             artist.AddLayer(newPaintedSprite);
+            paintedSprites.Add(newPaintedSprite);
 
             PaintedSpriteConstraint constraint = newDrop.AddComponent<PaintedSpriteConstraint>();
             constraint.Init(artist, spriteName, pixelsPerMeter, newDrop.transform);
+
+            newDrop.SetActive(false);
+            newPaintedSprite.scale = Vector2.zero;
         }
+
+        //drops[0].enabled = true;
+        //++numEnabledDrops;
     }
 
     private void Update()
     {
-        
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime >= spawnDelay && numEnabledDrops < numDrops)
+        {
+            drops[numEnabledDrops].Reset();
+            drops[numEnabledDrops].transform.position = transform.position;
+            drops[numEnabledDrops++].gameObject.SetActive(true);
+            elapsedTime = 0;
+        }
+
+        for (int i = 0; i < drops.Count; i++)
+        {
+            WaterDrop drop = drops[i];
+
+            if (!drop.gameObject.activeInHierarchy) break;
+
+            if(drop.TimeAlive > dropLifetime)
+            {
+                drop.Reset();
+                drop.transform.position = transform.position;
+            }
+        }
     }
 }
