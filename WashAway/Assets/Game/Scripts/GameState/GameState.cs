890aaca302b22c349871e0e8f73fa1a93237b25e
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-08-28 16:53:46 -0700

    Refactored Game State to prevent changing state in edit mode.

--- WashAway/Assets/Game/Scripts/GameState/GameState.cs
+++ WashAway/Assets/Game/Scripts/GameState/GameState.cs
@@ -1,86 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameState
+public class GameState
 {
-    public delegate void OnStateChange();
+    public delegate void OnToggle();
 
-    private static bool paused = false;
-    public static event OnStateChange onTogglePause;
-    public static bool  Paused 
-    { 
-        get 
-        { 
-            return paused; 
-        } 
-        set 
+    private class ToggleState
+    {
+        public bool value;
+        private bool defaultValue;
+        public event OnToggle onToggle;
+
+        public ToggleState(bool value = false)
         {
-            bool previousValue = paused;
+            this.value = value;
+            defaultValue = value;
+        }
 
-            paused = value;
-
-            if(previousValue != paused)
-            {
-                onTogglePause?.Invoke();
-            }
-        } 
+        public bool DefaultValue { get { return defaultValue; } }
+        
+        public void ValueChanged() { onToggle?.Invoke(); }
     }
 
-    private static bool currentLevelClear = false;
-    public static event OnStateChange onToggleCurrentLevelClear;
-    public static bool CurrentLevelClear
+    private static ToggleState paused = new ToggleState();
+    public static bool Paused { get { return ReadToggleState(paused); } set { ChangeToggleState(paused, value); } }
+    public static event OnToggle onTogglePause { add { paused.onToggle += value; } remove { paused.onToggle -= value; } }
+
+    private static ToggleState currentLevelClear = new ToggleState();
+    public static bool CurrentLevelClear { get { return ReadToggleState(currentLevelClear); } set { ChangeToggleState(currentLevelClear, value); } }
+    public static event OnToggle onToggleCurrentLevelClear { add { currentLevelClear.onToggle += value; } remove { currentLevelClear.onToggle -= value; } }
+
+    private static ToggleState feedbackViewed = new ToggleState();
+    public static bool FeedbackViewed { get { return ReadToggleState(feedbackViewed); } set { ChangeToggleState(feedbackViewed, value); } }
+    public static event OnToggle onToggleFeedbackViewed { add { feedbackViewed.onToggle += value; } remove { feedbackViewed.onToggle -= value; } }
+
+    private static ToggleState characterDead = new ToggleState();
+    public static bool CharacterDead { get { return ReadToggleState(characterDead); } set { ChangeToggleState(characterDead, value); } }
+    public static event OnToggle onToggleCharacterDead { add { characterDead.onToggle += value; } remove { characterDead.onToggle -= value; } }
+
+    private static void ChangeToggleState(ToggleState toggleState, bool newValue)
     {
-        get { return currentLevelClear; }
-        set
+        if(!CheckApplicationIsPlaying())
         {
-            bool previousValue = currentLevelClear;
-
-            currentLevelClear = value;
-
-            if(previousValue != currentLevelClear)
-            {
-                onToggleCurrentLevelClear?.Invoke();
-            }
+            return;
         }
+
+        bool previousValue = toggleState.value;
+
+        toggleState.value = newValue;
+
+        if (newValue != previousValue)
+        {
+            toggleState.ValueChanged();
+        }
+    }   
+    
+    private static bool ReadToggleState(ToggleState toggleState)
+    {
+        if(!CheckApplicationIsPlaying()) return toggleState.DefaultValue;
+
+        return toggleState.value;
     }
 
-    private static bool feedbackViewed = false;
-    public static event OnStateChange onToggleFeedbackViewed;
-    public static bool FeedbackViewed
+    private static bool CheckApplicationIsPlaying()
     {
-        get { return feedbackViewed; }
-        set
+        bool result = Application.isPlaying;
+        if (!result)
         {
-            bool previousValue = feedbackViewed;
-
-            feedbackViewed = value;
-
-            if (previousValue != feedbackViewed)
-            {
-                onToggleFeedbackViewed?.Invoke();
-            }
+            Debug.LogWarning("Tried to access Game State in edit mode. Will use default value for state.");
         }
-    }
 
-    private static bool startScreenRead = false;
-    public static bool StartScreenRead { get { return startScreenRead; } set {  startScreenRead = value; } }
-
-    private static bool characterDead = false;
-    public static event OnStateChange onToggleCharacterDead;
-    public static bool CharacterDead
-    {
-        get { return characterDead; }
-        set
-        {
-            bool previousValue = characterDead;
-
-            characterDead = value;
-
-            if (previousValue != characterDead)
-            {
-                onToggleCharacterDead?.Invoke();
-            }
-        }
+        return result;
     }
 }
