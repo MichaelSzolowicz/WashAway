7d35d2da170a05f73ef0c611e499d9601efd9967
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-09-19 18:12:10 -0700

    Created mask percentage calculator class.

---
+++ WashAway/Assets/Game/Scripts/MaskPercentCalculator/MaskPercentCalculator.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class MaskPercentCalculator
+{
+    public static void RequestPercentCleared(RenderTexture mask, Action<float> onCompleteRequest)
+    {
+        
+    }
+}
