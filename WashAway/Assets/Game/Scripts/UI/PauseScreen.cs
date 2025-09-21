2be72936d284fc09f1da709d3d608a36c3c43996
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-09-20 17:21:35 -0700

    Added pause screen script.

---
+++ WashAway/Assets/Game/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text maskPercentText;
+
+    private const string maskPercentLabel = "Cleared: ";
+
+    public void EnableAsPauseScreen(float maskPercent, float requiredPercent)
+    {
+        titleText.text = "Paused";
+        FinishEnable(maskPercent, requiredPercent);
+    }
+
+    public void EnableAsDeathScreen(float maskPercent, float requiredPercent)
+    {
+        titleText.text = "Try Again";
+        FinishEnable(maskPercent, requiredPercent);
+    }
+
+    public void EnableAsVictoryScreen(float maskPercent, float requiredPercent)
+    {
+        titleText.text = "Level Clear";
+        FinishEnable(maskPercent, requiredPercent);
+    }
+
+    private void FinishEnable(float maskPercent, float requiredPercent)
+    {
+        maskPercentText.text = maskPercentLabel + maskPercent + " / " + requiredPercent + "%";
+        gameObject.SetActive(true);
+    }
+}
