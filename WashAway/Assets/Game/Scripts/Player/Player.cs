ef06cd841ee5ac6f6008d39adb5c5cd83ac94d24
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-09-21 18:42:41 -0700

    Pause screen displays two decimals of clear percent.

--- WashAway/Assets/Game/Scripts/Player/Player.cs
+++ WashAway/Assets/Game/Scripts/Player/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
     {
         if (
             Input.GetKeyDown(KeyCode.Escape)
-            && !GameState.CurrentLevelClear
+            && !GameState.CharacterDead
             )
         {
             GameState.Paused = !GameState.Paused;
