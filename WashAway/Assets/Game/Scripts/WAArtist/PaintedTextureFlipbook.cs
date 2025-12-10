54b58d7c188b0d5ce4d60a1a6f3761470d1e6a0b
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-12-09 22:40:52 -0800

    Setup animation in blended detail maps test scene.

--- WashAway/Assets/Game/Scripts/WAArtist/PaintedTextureFlipbook.cs
+++ WashAway/Assets/Game/Scripts/WAArtist/PaintedTextureFlipbook.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class PaintedTextureFlipbook : PaintedTextureHook
 {
     [SerializeField] private List<Texture> sourceTextures = new List<Texture>();
+    [SerializeField] private int frequency = 12;
+    private int currentFrame = 0;
+    private float elapsedTime = 0;
 
     public override Texture GetTexture()
     {
-        return sourceTextures[0];
+        if (!Application.isPlaying)
+        {
+            elapsedTime = 0.0f;
+            currentFrame = 0;
+            return sourceTextures[currentFrame];
+        }
+
+        elapsedTime += Time.deltaTime;
+        float min = (1.0f / frequency);
+        if (elapsedTime >= min) 
+        {
+            currentFrame++;
+            if (currentFrame >= sourceTextures.Count) { currentFrame = 0; }
+            elapsedTime = 0;
+        }
+
+        return sourceTextures[currentFrame];
     }
 }
