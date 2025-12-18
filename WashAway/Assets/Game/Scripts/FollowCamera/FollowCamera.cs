13f8e982e4690bddeb0128ea63aaba7a1e1e7abe
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2025-12-18 14:14:52 -0800

    Follow cam offset now dependent only on child objects, not code.

--- WashAway/Assets/Game/Scripts/FollowCamera/FollowCamera.cs
+++ WashAway/Assets/Game/Scripts/FollowCamera/FollowCamera.cs
@@ -5,18 +5,11 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float lagTime;
 
-    private Vector3 startOffset;
-
     private Vector3 velocity = Vector3.zero;
-
-    void Start()
-    {
-        startOffset = transform.position - target.position;
-    }
 
     void LateUpdate()
     {
-        Vector3 newPosition = Vector3.SmoothDamp(transform.position, target.position + startOffset, ref velocity, lagTime);
+        Vector3 newPosition = Vector3.SmoothDamp(transform.position, target.position, ref velocity, lagTime);
         newPosition.z = transform.position.z;
         transform.position = newPosition;
     }
