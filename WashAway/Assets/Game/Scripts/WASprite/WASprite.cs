fefb76adff32845a7a8660dee1a144b0f9e0497b
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2026-03-27 17:22:28 -0700

    WASprite creates renderer objects for newly assign sprites.

--- WashAway/Assets/Game/Scripts/WASprite/WASprite.cs
+++ WashAway/Assets/Game/Scripts/WASprite/WASprite.cs
@@ -9,8 +9,10 @@ public class WASprite : MonoBehaviour
     [SerializeField] private Sprite roughSprite;
     [SerializeField] private Sprite thickSprite;
 
-    [SerializeField, HideInInspector] private SpriteRenderer colorRenderer;
-    [SerializeField, HideInInspector] private SpriteRenderer normalRenderer;
-    [SerializeField, HideInInspector] private SpriteRenderer roughRenderer;
-    [SerializeField, HideInInspector] private SpriteRenderer thickRenderer;
+    // TODO: visible for testing purpose, should be hidden in inspector.
+    [SerializeField] private SpriteRenderer colorRenderer;
+    [SerializeField] private SpriteRenderer normalRenderer;
+    [SerializeField] private SpriteRenderer roughRenderer;
+    [SerializeField] private SpriteRenderer thickRenderer;
+
 }
--- WashAway/Assets/Game/Scripts/WASprite/WASpriteEditor.cs
+++ WashAway/Assets/Game/Scripts/WASprite/WASpriteEditor.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 [CustomEditor(typeof(WASprite))]
 public class WASpriteEditor : Editor
 {
-    private const int SPRITE_STRING_LENGTH = 6;
     private const string SPRITE = "Sprite";
+    private const string RENDERER = "Renderer";
 
     private WASprite waSprite;
 
@@ -28,7 +29,7 @@ public class WASpriteEditor : Editor
         while (nextProperty.NextVisible(false))
         {
             string checkSubname = nextProperty.name.Length >= 6 ? 
-                nextProperty.name.Substring(nextProperty.name.Length - SPRITE_STRING_LENGTH, SPRITE_STRING_LENGTH) : 
+                nextProperty.name.Substring(nextProperty.name.Length - SPRITE.Length, SPRITE.Length) : 
                 nextProperty.name;
 
             switch(checkSubname)
@@ -50,8 +51,92 @@ public class WASpriteEditor : Editor
 
     }
 
-    private void HandleSprite(SerializedProperty sprite)
+    private void HandleSprite(SerializedProperty spriteProperty)
     {
+        Object oldValue = spriteProperty.objectReferenceValue;
 
+        EditorGUILayout.PropertyField(spriteProperty);
+
+        Object newValue = spriteProperty.objectReferenceValue;
+
+        if (oldValue == newValue) return;
+
+        string layer = spriteProperty.name.Substring(0, spriteProperty.name.Length - SPRITE.Length);
+        string rendererPropertyName = layer + RENDERER;
+
+        SerializedProperty rendererProperty = serializedObject.FindProperty(rendererPropertyName);
+        if (rendererProperty == null)
+        {
+            Debug.LogError("No serialized property \"" + layer + SPRITE + "\" found on serialized object \"" + serializedObject.targetObject.name + "\"");
+            return;
+        }
+
+        SpriteRenderer rendererComponent = (SpriteRenderer)rendererProperty.objectReferenceValue;
+
+        if(rendererComponent != null)
+        {
+            // Case: renderer already set up, safe to assign sprite.
+            rendererComponent.sprite = (Sprite)newValue;
+            return;
+        }
+
+        // Case: no renderer found, create one.
+        CreateRenderer(rendererProperty, (Sprite)newValue);
+    }
+
+    private void CreateRenderer(SerializedProperty rendererProperty, Sprite newValue)
+    {
+        // TODO: Should implement search for already exisitng child objects?
+
+        // We will be creating a new object to hold the renderer component.
+        string rendererPropertyName = rendererProperty.name;
+        string rendererObjectName = char.ToUpper(rendererPropertyName[0]) + rendererPropertyName.Substring(1);
+        string layer = rendererObjectName.Substring(0, rendererObjectName.Length - RENDERER.Length);
+
+        GameObject rendererObject = new GameObject(rendererObjectName);
+
+        rendererObject.layer = LayerMask.NameToLayer(layer);
+
+        SpriteRenderer rendererComponent = rendererObject.AddComponent<SpriteRenderer>();
+        rendererComponent.sprite = newValue;
+
+        // Editing a prefab asset?
+        if (PrefabUtility.IsPartOfPrefabAsset(waSprite))
+        {
+            // Add child object to prefab asset
+            string prefabAssetPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(waSprite);
+
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabAssetPath);
+            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+            rendererObject.transform.SetParent(instance.transform, false);
+
+            PrefabUtility.ApplyPrefabInstance(instance, InteractionMode.AutomatedAction);
+            DestroyImmediate(instance);
+
+            // Link renderer to wasprite
+            Transform rendererTransform = prefab.transform.Find(rendererObjectName);
+            if (rendererTransform == null)
+            {
+                Debug.LogError("Could not find child object of name \"" + rendererObjectName + "\" in prefab at \"" + prefabAssetPath + "\"");
+                return;
+            }
+
+            rendererComponent = rendererTransform.GetComponent<SpriteRenderer>();
+            if (rendererComponent == null)
+            {
+                Debug.LogError("Could not find component of type \"" + typeof(SpriteRenderer).Name + "\" on child object \"" + rendererTransform.gameObject.name + "\" of prefab at path \"" + prefabAssetPath + "\"");
+                return;
+            }
+
+            rendererProperty.objectReferenceValue = rendererComponent;
+
+            return;
+        }
+
+        // Editing a object in scene
+        rendererObject.transform.SetParent(waSprite.transform, false);
+
+        rendererProperty.objectReferenceValue = rendererComponent;
     }
 }
